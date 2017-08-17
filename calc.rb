6daae09ef9178c1bcd0add7714bc9d43620d19e7
Ty-Zeta <tyofzeta@gmail.com>
2017-08-17 15:18:28 -0400

    updated with comments

--- calc.rb
+++ calc.rb
@@ -1,4 +1,6 @@
-def calculator (calc, num1, num2)
+def calculator(calc, num1, num2)
+    # function witht the arguement of the choice of radio button and the two numbers entered
+
     if  calc == "plus"
         answer = num1.to_i + num2.to_i
     end
@@ -14,5 +16,9 @@ def calculator (calc, num1, num2)
     if calc == "divided by"
         answer = num1.to_i / num2.to_i
     end
+    #num1 & 2 have to be integers or otherwise they will not pass
+
     answer.to_s
+    # the answer to the math problem needs to be a string in order to pass
+    
 end
